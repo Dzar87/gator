58735e019a6e0a316eac584dbf2c04ef0597436a
Dzar87 <7062912+Dzar87@users.noreply.github.com>
2026-05-02 13:39:44 +0300

    Add missing annotation ends

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -2,13 +2,14 @@
 INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
 VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
-
+--
 -- name: GetFeedByURL :one
 SELECT * FROM feeds WHERE url = $1;
-
+--
 -- name: GetFeedsWithUser :many
 SELECT
     sqlc.embed(f),
     sqlc.embed(u)
 FROM feeds f
 JOIN users u ON f.user_id = u.id;
+--
