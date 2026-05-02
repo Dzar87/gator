349f6c81edc3bb3d5dd30f1ffddb646293d12b2d
Dzar87 <7062912+Dzar87@users.noreply.github.com>
2026-05-02 16:07:00 +0300

    Extend agg command

--- internal/cli/handler.go
+++ internal/cli/handler.go
@@ -125,12 +125,27 @@ func handlerUsers(ctx context.Context, s *state.State, cmd Command) error {
 }
 
 func handlerAggregate(ctx context.Context, s *state.State, cmd Command) error {
-	url := "https://www.wagslane.dev/index.xml"
-	feed, err := rss.FetchFeed(ctx, url)
-	if err != nil {
-		return fmt.Errorf("agg: failed to fetch feed: %w", err)
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <time_between_reqs>", cmd.Name)
 	}
-	fmt.Printf("Feed: %+v\n", feed)
+	interval, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("agg: failed to parse interval: %w", err)
+	}
+	ticker := time.NewTicker(interval)
+	fmt.Println("Collecting feeds every", interval)
+	for {
+		if err := scrapeFeeds(ctx, s); err != nil {
+			s.Logger.Error("Failed to scrape feed", "err", err)
+		}
+		select {
+			case <- ctx.Done():
+				ticker.Stop()
+				return nil
+			case <-ticker.C:
+				continue
+		}
+	}
 	return nil
 }
 
