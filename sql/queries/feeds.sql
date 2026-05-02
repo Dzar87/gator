8de885b9f782d140fdc4daabd7d936bd3ee1fc32
Dzar87 <7062912+Dzar87@users.noreply.github.com>
2026-05-02 14:56:46 +0300

    Add GetNextFeedToFetch queries

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -19,3 +19,8 @@ SET last_fetched_at = NOW(), updated_at = NOW()
 WHERE feeds.id = $1
 RETURNING *;
 --
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY feeds.last_fetched_at ASC NULLS FIRST
+LIMIT 1;
+--
