d0e7742affd5fed4355766a93afee94ad865e7f4
Dzar87 <7062912+Dzar87@users.noreply.github.com>
2026-05-02 23:47:14 +0300

    Fix updated_at default

--- sql/schema/003_feed_follows.sql
+++ sql/schema/003_feed_follows.sql
@@ -3,7 +3,7 @@ CREATE TABLE feed_follows (
     id UUID PRIMARY KEY DEFAULT (uuidv7()),
     created_at TIMESTAMPTZ NOT NULL
     GENERATED ALWAYS AS (uuid_extract_timestamp("id")) STORED,
-    updated_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
     CONSTRAINT uniq_user_id_feed_id UNIQUE (user_id, feed_id)
